998c9503d7eecb10b5c44cade6392991f258c723
Guscosta88 <guslfcosta@gmail.com>
2022-02-08 09:27:44 +1100

    navbar added, display account and biometrics pages added

--- account.php
+++ account.php
@@ -9,7 +9,7 @@ $user_id = $_SESSION['user_id'];
 
   if (isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
-    $sql = "select * from macros where user_id=".$user_id;
+    $sql = "select * from users where user_id=".$user_id;
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
       $row = mysqli_fetch_assoc($result);
@@ -18,10 +18,11 @@ $user_id = $_SESSION['user_id'];
     }
   }
 
-    $calories = $row['calories'];
-    $protein = $row['protein'];
-    $fat = $row['fat'];
-    $carbohydrates = $row['carbohydrates'];
+    $first_name = $row['first_name'];
+    $last_name = $row['last_name'];
+    $email = $row['email'];
+    $address = $row['address'];
+    $phone = $row['phone'];
 
 ?>
 
@@ -33,7 +34,7 @@ $user_id = $_SESSION['user_id'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <title>Meal</title>
+    <title>Account</title>
 </head>
 <body>
   <header>
@@ -58,11 +59,12 @@ $user_id = $_SESSION['user_id'];
     <?php echo 
     "<h1> Hello " . $sess . " </h1><br />
     <div class='macros'>
-    <strong> Your Macros are: </strong><br />
-    <div> Calories: " . $calories . "Kcal </div><br /> 
-    <div>Protein: " . $protein ."g </div><br /> 
-    <div>Fat: " .$fat . "g </div><br /> 
-    <div>Carbohydrates: " . $carbohydrates . "g 
+    <strong> Your Account info: </strong><br />
+    <div> First Name: " . $first_name . "</div><br /> 
+    <div> Last Name: " . $last_name . " </div><br /> 
+    <div>Email: " . $email ."</div><br /> 
+    <div>Address: " .$address . "</div><br /> 
+    <div>Phone: " . $phone . "
     </div>"; ?>
     
     <a href="Logout.php">Log Out</a>
--- biometrics.php
+++ biometrics.php
@@ -9,7 +9,7 @@ $user_id = $_SESSION['user_id'];
 
   if (isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
-    $sql = "select * from macros where user_id=".$user_id;
+    $sql = "select * from user_data where user_id=".$user_id;
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
       $row = mysqli_fetch_assoc($result);
@@ -18,10 +18,12 @@ $user_id = $_SESSION['user_id'];
     }
   }
 
-    $calories = $row['calories'];
-    $protein = $row['protein'];
-    $fat = $row['fat'];
-    $carbohydrates = $row['carbohydrates'];
+    $weight = $row['weight'];
+    $height = $row['height'];
+    $age = $row['age'];
+    $sex = $row['sex'];
+    $activity = $row['activity'];
+    $goal = $row['goal'];
 
 ?>
 
@@ -33,7 +35,7 @@ $user_id = $_SESSION['user_id'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <title>Meal</title>
+    <title>Biometrics</title>
 </head>
 <body>
   <header>
@@ -58,11 +60,13 @@ $user_id = $_SESSION['user_id'];
     <?php echo 
     "<h1> Hello " . $sess . " </h1><br />
     <div class='macros'>
-    <strong> Your Macros are: </strong><br />
-    <div> Calories: " . $calories . "Kcal </div><br /> 
-    <div>Protein: " . $protein ."g </div><br /> 
-    <div>Fat: " .$fat . "g </div><br /> 
-    <div>Carbohydrates: " . $carbohydrates . "g 
+    <strong> Your Biometrics are: </strong><br />
+    <div> Weight: " . $weight . "Kg </div><br /> 
+    <div> Height: " . $height . "cm </div><br /> 
+    <div>Age: " . $age ."</div><br /> 
+    <div>Sex: " .$sex . "</div><br /> 
+    <div>Activity: " . $activity . " </div><br />
+    <div>Goal: " . $goal . " weight
     </div>"; ?>
     
     <a href="Logout.php">Log Out</a>
