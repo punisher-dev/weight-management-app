c8270943bace5d383b91e30fb78ae80b1a5e64ab
Guscosta88 <guslfcosta@gmail.com>
2022-03-14 14:50:24 +1100

    form data separated from script using ajax

---
+++ main.js
@@ -0,0 +1,38 @@
+$('form.ajax').on('submit', function(){
+    var that = $(this),
+        url = that.attr('action'),
+        type = that.attr('method'),
+        data = {};
+
+        that.find('[name]').each(function(index, value){
+            var that = $(this),
+                name = that.attr('name'),
+                value = that.val();
+
+                data[name] = value;
+        });
+
+        $.ajax({
+            url: url,
+            type: type,
+            data: data,
+            success: function(response) {
+                console.log(response);
+            }
+        });
+        return false;
+});
+
+$(document).ready(function(){
+    $("#show").click(function(e){
+      e.preventDefault();
+      $(".shown").fadeIn();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#show").click(function(e){
+      e.preventDefault();
+      $(".edit").hide();
+    });
+  });
--- register.php
+++ register.php
@@ -4,7 +4,7 @@ include 'Config.php';
 session_start();
 
 if(isset($_SESSION['first_name'])){
-    header("Location: index.php");
+    header("Location: index.html");
 }
 
 $first_name = '';
@@ -35,9 +35,9 @@ if(isset($_POST['submit'])){
                         VALUES(:first_name, :last_name, :email, :address, :phone, :created_at, :password)";
         $stmt = $conn->prepare($sql);
         $stmt->execute(['first_name' => $first_name, 'last_name' => $last_name, 'email' => $email, 'address' => $address, 'phone' => $phone, 'created_at' => $created_at, 'password' => $password]);
-        header("Location: index.php");
+        header("Location: index.html");
         if($result){
-        header("Location: index.php");
+        header("Location: index.html");
         $first_name = "";
         $last_name = "";
         $email = "";
@@ -57,63 +57,3 @@ if(isset($_POST['submit'])){
  }
 
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>Register</title>
-
-</head>
-<body>
-
-<div class="wrapper">
-<div class="card">
-    <h1> REGISTER </h1>
-<form action="" method="POST" >
-    <div class="mb-3">
-            <label for="first_name" class="form-label">First Name:*</label>
-            <input class="form-control" type="text" name="first_name" placeholder="First Name" value="<?php echo $first_name; ?>" />
-    </div>
-    <div class="mb-3">
-            <label for="last_name" class="form-label">Last Name:*</label>
-            <input class="form-control" type="text" name="last_name" placeholder="Last Name" value="<?php echo $last_name; ?>" />
-    </div>
-    <div class="mb-3">
-            <label for="email" class="form-label">Email:*</label>
-            <input class="form-control" type="email" name="email" placeholder="Email" value="<?php echo $email; ?>" />
-    </div>
-    <div class="mb-3">
-            <label for="address" class="form-label">Address:*</label>
-            <input class="form-control" type="text" name="address" placeholder="Address" value="<?php echo $address; ?>" />
-    </div>
-    <div class="mb-3">
-            <label for="phone" class="form-label">Phone:*</label>
-            <input class="form-control" type="text" name="phone" placeholder="Phone" value="<?php echo $phone; ?>" />
-    </div>
-    <div class="mb-3">
-            <label for="password" class="form-label">Password:*</label>
-            <input class="form-control" type="password" name="password" />
-    </div>
-    <div class="mb-3">
-            <label for="cpassword" class="form-label">Confirm Password:*</label>
-            <input class="form-control" type="password" name="cpassword" />
-    </div>
-<button class="btn btn-secondary" type="submit" name="submit">Register</button>
-
-
-<p> Have an account?
-    <a href="index.php">Login Here</a>
-</p>
-
-</form>
-</div>
-</div>
-</body>
-</html>
