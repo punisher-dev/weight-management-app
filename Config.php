b1e1a7d00bef8148c2f74d8a020acaf59eaf804a
Guscosta88 <guslfcosta@gmail.com>
2022-05-20 12:14:03 +1000

    Added config

--- Config.php
+++ Config.php
@@ -1,5 +1,22 @@
 <?php
-$servername = "localhost";
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
+
+
+
+
+
+
+<!-- $servername = "localhost";
 $username = "root";
 $pass = "";
 $db = "weight_management_app";
@@ -13,7 +30,6 @@ $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 if (!$conn){
     die("<script>alert('Connection Failed')</script>");
-}
+} -->
 
 
-?>
