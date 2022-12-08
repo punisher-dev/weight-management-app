40b457c3ad4683cd0fbd8be8daf47411534fd8e9
Guscosta88 <guslfcosta@gmail.com>
2022-12-08 21:26:12 +1100

    config updated

--- Config.php
+++ Config.php
@@ -1,28 +1,21 @@
 <?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
+$mySqlDatabase = "railway";
+$mySqlHost = "containers-us-west-158.railway.app";
+$pass = "cuBKMzc0CxtocPIQ3qLF";
+$mySqlPort = "7431";
+$mySqlUser = "root";
 
-$dsn = 'mysql:host='. $cleardb_server .';dbname='. $cleardb_db;
+$dsn = 'mysql://'. $mySqlUser .':'. $pass .'@'.$mySqlHost.':'.$mySqlPort.'/'.$mySqlDatabase;
 
-$conn = new PDO($dsn, $cleardb_username, $cleardb_password);
+$conn = new PDO($dsn, $mySqlUser, $pass);
 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 
 if (!$conn){
     die("<script>alert('Connection Failed')</script>");
-}
-
-
+} 
 
 
 ?>
@@ -32,20 +25,6 @@ if (!$conn){
 
 
 
-<!-- $servername = "localhost";
-$username = "root";
-$pass = "";
-$db = "weight_management_app";
 
-$dsn = 'mysql:host='. $servername .';dbname='. $db;
-
-$conn = new PDO($dsn, $username, $pass);
-$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-
-if (!$conn){
-    die("<script>alert('Connection Failed')</script>");
-} -->
 
 
