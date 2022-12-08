32423dd3c49782476512a04329d36170ad6b0687
Guscosta88 <guslfcosta@gmail.com>
2022-12-08 21:54:18 +1100

    test5

--- Config.php
+++ Config.php
@@ -1,25 +1,22 @@
 <?php
 
-// $mySqlDatabase = "railway";
-// $mySqlHost = "containers-us-west-158.railway.app";
-// $pass = "cuBKMzc0CxtocPIQ3qLF";
-// $mySqlPort = "7431";
-// $mySqlUser = "root";
+$mySqlDatabase = "railway";
+$mySqlHost = "containers-us-west-158.railway.app";
+$pass = "cuBKMzc0CxtocPIQ3qLF";
+$mySqlPort = "7431";
+$mySqlUser = "root";
 
-// $dsn = 'mysql://'. $mySqlUser .':'. $pass .'@'.$mySqlHost.':'.$mySqlPort.'/'.$mySqlDatabase;
+$dsn = 'mysql://'. $mySqlUser .':'. $pass .'@'.$mySqlHost.':'.$mySqlPort.'/'.$mySqlDatabase;
 
-// print_r($dsn)
-
-// $conn = new PDO($dsn, $mySqlUser, $pass);
-// $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-// $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$conn = new PDO($dsn, $mySqlUser, $pass);
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 
-// if (!$conn){
-//     die("<script>alert('Connection Failed')</script>");
-// } 
+if (!$conn){
+    die("<script>alert('Connection Failed')</script>");
+} 
 
-mysql -hcontainers-us-west-158.railway.app -uroot -pcuBKMzc0CxtocPIQ3qLF --port 7431 --protocol=TCP railway
 
 
 ?>
