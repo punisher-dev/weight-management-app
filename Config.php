86e60951b30bfa1a8cbb7074ada393a91f6a2776
Guscosta88 <guslfcosta@gmail.com>
2022-12-09 09:17:22 +1100

    test7

--- Config.php
+++ Config.php
@@ -1,21 +1,62 @@
 <?php
 
-$mySqlDatabase = "railway";
-$mySqlHost = "containers-us-west-158.railway.app";
+// $mySqlDatabase = "railway";
+// $mySqlHost = "containers-us-west-158.railway.app";
+// $pass = "cuBKMzc0CxtocPIQ3qLF";
+// $mySqlPort = "7431";
+// $mySqlUser = "root";
+
+// $dsn = 'mysql://'. $mySqlUser .':'. $pass .'@'.$mySqlHost.':'.$mySqlPort.'/'.$mySqlDatabase;
+
+// $conn = new PDO($dsn, $mySqlUser, $pass);
+// $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+// $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+
+// if (!$conn){
+//     die("<script>alert('Connection Failed')</script>");
+// } 
+
+// ____________________________________
+
+// $servername = "localhost";
+// $username = "root";
+// $pass = "";
+// $db = "weight_management_app";
+
+// $conn = mysqli_connect($servername, $username, $pass, $db);
+// $dsn = 'mysql:host='. $servername .';dbname='. $db;
+
+// $conn = new PDO($dsn, $username, $pass);
+// $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+// $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+
+// if (!$conn){
+//     die("<script>alert('Connection Failed')</script>");
+// }
+
+
+// ____________________________________
+
+
+
+$servername = "containers-us-west-158.railway.app";
+$username = "root";
 $pass = "cuBKMzc0CxtocPIQ3qLF";
-$mySqlPort = "7431";
-$mySqlUser = "root";
+$db = "railway";
 
-$dsn = 'mysql://'. $mySqlUser .':'. $pass .'@'.$mySqlHost.':'.$mySqlPort.'/'.$mySqlDatabase;
+$conn = mysqli_connect($servername, $username, $pass, $db);
+$dsn = 'mysql:host='. $servername .';dbname='. $db;
 
-$conn = new PDO($dsn, $mySqlUser, $pass);
+$conn = new PDO($dsn, $username, $pass);
 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 
 if (!$conn){
     die("<script>alert('Connection Failed')</script>");
-} 
+}
 
 
 
