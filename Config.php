9a340e858b43fe36ef6170957279a82f252db086
Guscosta88 <guslfcosta@gmail.com>
2022-12-11 12:57:45 +1100

    test18

--- Config.php
+++ Config.php
@@ -53,15 +53,13 @@ $dsn = 'mysql://'.$username.':'.$pass.'@'.$servername.':5475/'.$db.'';
 
 
 $conn = new PDO($dsn, $username, $pass);
-$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+// $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+// $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 
 if (!$conn){
     die("<script>alert('Connection Failed')</script>");
 }
-
-
 
 ?>
 
